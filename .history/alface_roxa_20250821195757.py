e5bbcaebec96134fa20010698f7876f8650d3b10
Alessandra <alessandravitoria799@gmail.com>
2025-08-21 19:58:59 -0300

    Commit inicial

---
+++ .history/alface_roxa_20250821195757.py
@@ -0,0 +1,11 @@
+"""
+==================================================
+Script Name : alface_roxa.py.py
+Author      : Alessandra
+Date        : 2025-08-21
+Description : 
+Usage       : 
+Dependencies: 
+Python ver. : 3.x
+==================================================
+"""
